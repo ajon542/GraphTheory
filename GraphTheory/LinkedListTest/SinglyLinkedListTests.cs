223ac2a40e3779734a3583466596f856935fa8c6
Andrew Jones <ajon542@gmail.com>
2016-08-22 19:11:20 -0700

    Added singly linked list

---
+++ GraphTheory/LinkedListTest/SinglyLinkedListTests.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using GraphTheory;
+
+namespace LinkedListTest
+{
+    [TestClass]
+    public class SinglyLinkedListTests
+    {
+        #region AddHead Tests
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.AddHead(head, 1);
+
+            Assert.AreEqual(1, head.data);
+            Assert.IsNull(head.next);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.AddHead(head, 1);
+            head = SinglyLinkedList.AddHead(head, 2);
+
+            Assert.AreEqual(2, head.data);
+            Assert.AreEqual(1, head.next.data);
+            Assert.IsNull(head.next.next);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.AddHead(head, 1);
+            head = SinglyLinkedList.AddHead(head, 2);
+            head = SinglyLinkedList.AddHead(head, 3);
+
+            Assert.AreEqual(3, head.data);
+            Assert.AreEqual(2, head.next.data);
+            Assert.AreEqual(1, head.next.next.data);
+            Assert.IsNull(head.next.next.next);
+        }
+        #endregion
+
+        #region AddTail Tests
+        [TestMethod]
+        public void TestMethod4()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.AddTail(head, 1);
+
+            Assert.AreEqual(1, head.data);
+            Assert.IsNull(head.next);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.AddTail(head, 1);
+            head = SinglyLinkedList.AddTail(head, 2);
+
+            Assert.AreEqual(1, head.data);
+            Assert.AreEqual(2, head.next.data);
+            Assert.IsNull(head.next.next);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.AddTail(head, 1);
+            head = SinglyLinkedList.AddTail(head, 2);
+            head = SinglyLinkedList.AddTail(head, 3);
+
+            Assert.AreEqual(1, head.data);
+            Assert.AreEqual(2, head.next.data);
+            Assert.AreEqual(3, head.next.next.data);
+            Assert.IsNull(head.next.next.next);
+        }
+        #endregion
+
+        #region Insert Tests
+        [TestMethod]
+        public void TestMethod7()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.Insert(head, 1, 0);
+
+            Assert.AreEqual(1, head.data);
+            Assert.IsNull(head.next);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.Insert(head, 1, 0);
+            head = SinglyLinkedList.Insert(head, 2, 0);
+
+            Assert.AreEqual(2, head.data);
+            Assert.AreEqual(1, head.next.data);
+            Assert.IsNull(head.next.next);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.Insert(head, 1, 0);
+            head = SinglyLinkedList.Insert(head, 2, 0);
+            head = SinglyLinkedList.Insert(head, 3, 1);
+
+            Assert.AreEqual(2, head.data);
+            Assert.AreEqual(3, head.next.data);
+            Assert.AreEqual(1, head.next.next.data);
+            Assert.IsNull(head.next.next.next);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.Insert(head, 1, 0);
+            head = SinglyLinkedList.Insert(head, 2, 0);
+            head = SinglyLinkedList.Insert(head, 3, 2);
+
+            Assert.AreEqual(2, head.data);
+            Assert.AreEqual(1, head.next.data);
+            Assert.AreEqual(3, head.next.next.data);
+            Assert.IsNull(head.next.next.next);
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            SNode head = null;
+            head = SinglyLinkedList.Insert(head, 1, 0);
+            head = SinglyLinkedList.Insert(head, 2, 0);
+            head = SinglyLinkedList.Insert(head, 3, 100);
+
+            Assert.AreEqual(2, head.data);
+            Assert.AreEqual(1, head.next.data);
+            Assert.AreEqual(3, head.next.next.data);
+            Assert.IsNull(head.next.next.next);
+        }
+
+        #endregion
+    }
+}
--- GraphTheory/SinglyLinkedList.cs
+++ GraphTheory/SinglyLinkedList.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace GraphTheory
 {
-    class SinglyLinkedList
+    public class SNode
     {
+        public int data;
+        public SNode next;
+    }
+
+    public class SinglyLinkedList
+    {
+        public static SNode AddHead(SNode head, int data)
+        {
+            SNode newNode = new SNode { data = data };
+            newNode.next = head;
+            return newNode;
+        }
+
+        public static SNode AddTail(SNode head, int data)
+        {
+            SNode newNode = new SNode { data = data };
+
+            // List doesn't exist, just return the new node.
+            if (head == null)
+            {
+                return newNode;
+            }
+
+            SNode tmp = head;
+
+            // Iterate to end of list.
+            while (tmp.next != null)
+            {
+                tmp = tmp.next;
+            }
+
+            // Add the new node.
+            tmp.next = newNode;
+
+            return head;
+        }
+
+        public static SNode Insert(SNode head, int data, int position)
+        {
+            SNode newNode = new SNode { data = data };
+
+            // Adding position 0 is the same as AddHead.
+            if (position == 0)
+            {
+                return AddHead(head, data);
+            }
+
+            int index = 0;
+            SNode curr = head;
+            SNode prev = null;
+
+            while (curr != null && index < position)
+            {
+                prev = curr;
+                curr = curr.next;
+                index++;
+            }
+
+            // Add the new node.
+            newNode.next = curr;
+            prev.next = newNode;
+
+            return head;
+        }
     }
 }
