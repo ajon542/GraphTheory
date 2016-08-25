52459870bfdcf02912b512bc4ed903e6ed2d658f
Andrew Jones <ajon542@gmail.com>
2016-08-25 16:24:36 -0700

    Adding methods to doubly linked list

--- GraphTheory/DoublyLinkedList.cs
+++ GraphTheory/DoublyLinkedList.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace GraphTheory
 {
-    class DoublyLinkedList
+    public class DNode
     {
+        public int data;
+        public DNode next;
+        public DNode prev;
+    }
+
+    public class DoublyLinkedList
+    {
+        public static DNode AddHead(DNode head, int data)
+        {
+            DNode newNode = new DNode { data = data };
+            newNode.next = head;
+
+            if (head != null)
+            {
+                head.prev = newNode;
+            }
+
+            return newNode;
+        }
     }
 }
---
+++ GraphTheory/LinkedListTest/DoublyLinkedListTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using GraphTheory;
+
+namespace LinkedListTest
+{
+    [TestClass]
+    public class DoublyLinkedListTest
+    {
+        #region AddHead Tests
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DNode head = null;
+            head = DoublyLinkedList.AddHead(head, 1);
+
+            // 1 <-> null
+            Assert.AreEqual(1, head.data);
+            Assert.IsNull(head.next);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DNode head = null;
+            head = DoublyLinkedList.AddHead(head, 1);
+            head = DoublyLinkedList.AddHead(head, 2);
+
+            // 2 <-> 1 <-> null
+            Assert.AreEqual(2, head.data);
+            Assert.AreEqual(1, head.next.data);
+            Assert.AreEqual(2, head.next.prev.data);
+            Assert.IsNull(head.next.next);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DNode head = null;
+            head = DoublyLinkedList.AddHead(head, 1);
+            head = DoublyLinkedList.AddHead(head, 2);
+            head = DoublyLinkedList.AddHead(head, 3);
+
+            // 3 <-> 2 <-> 1 <-> null
+            Assert.AreEqual(3, head.data);
+            Assert.AreEqual(2, head.next.data);
+            Assert.AreEqual(3, head.next.prev.data);
+            Assert.AreEqual(1, head.next.next.data);
+            Assert.AreEqual(2, head.next.next.prev.data);
+            Assert.IsNull(head.next.next.next);
+        }
+        #endregion
+    }
+}
